2c97d3993061a733c8b617cada40641b8038036d
MazuttiGuilherme <mazuttimail@gmail.com>
2024-07-04 12:38:04 -0300

    First Commit

---
+++ src/test/java/projetos/spring_bank_api/SpringBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package projetos.spring_bank_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
